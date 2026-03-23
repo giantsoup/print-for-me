3b6995721a9a34104990cb6e76d7b3213cdcb348
Taylor <taylor@ufbahc.com>
2026-03-23 15:04:23 -0700

    Fix CI code style issues

--- app/Http/Middleware/EnforceSessionVersion.php
+++ app/Http/Middleware/EnforceSessionVersion.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Middleware;
 
-use Closure;
 use App\Models\User;
+use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
